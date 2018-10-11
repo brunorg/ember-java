72c139f10e70374a370e0c2c67a61afc9100f196
Bruno Roberto Gonçalves <goncalvesb@gmail.com>
2018-10-10 23:47:36 -0300

    Creating template for customer list

--- app/routes/customers.js
+++ app/routes/customers.js
@@ -1,4 +1,21 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model() {
+    return [{
+      id: 123,
+      firstName: "John",
+      lastName: "Doe",
+      orders: []
+    }, {
+      id: 456,
+      firstName: "Frodo",
+      lastName: "Baggins",
+      orders: []
+    }];
+  },
+  actions: {
+    remove(data) {
+    }
+  }
 });
