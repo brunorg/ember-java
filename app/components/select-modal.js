c1959fbd7b36da46b544bc6808bf70e4196a1190
Bruno Roberto Gonçalves <goncalvesb@gmail.com>
2018-12-08 22:29:46 -0200

    Creating a new modal component

---
+++ app/components/select-modal.js
@@ -0,0 +1,18 @@
+import Component from '@ember/component';
+
+export default Component.extend({
+  actions: {
+    launchModal() {
+      this.set('confirmShown', true);
+    },
+
+    submitConfirm() {
+      // trigger action on parent component
+      this.set('confirmShown', false);
+    },
+
+    cancelConfirm() {
+      this.set('confirmShown', false);
+    }
+  }
+});
