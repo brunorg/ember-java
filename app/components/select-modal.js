fca50c13f4691b40fb1b72df7c3cd32fb9ffb413
Bruno Roberto Gonçalves <goncalvesb@gmail.com>
2019-10-14 23:07:36 -0300

    Apply filter on Select ModalIncludes an input search that calls the filterAction attribute with the value of input search.

--- app/components/select-modal.js
+++ app/components/select-modal.js
@@ -1,7 +1,20 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+
+  value: '',
+
+  init() {
+    this._super(...arguments);
+    this.filter('').then((results) => this.set('results', results));
+  },
+
   actions: {
+    query() {
+      var query = this.get('search');
+      this.filter(query).then(results => this.set("results", results));
+    },
+
     launchModal() {
       this.set('confirmShown', true);
     },
