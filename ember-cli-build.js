be44788199e250bd1a59e513b0d24f9ba0f60c55
Bruno Roberto Gonçalves <goncalvesb@gmail.com>
2020-03-03 15:19:03 -0300

    Removing foundation layout

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,10 +5,6 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     // Add options here
-    'ember-cli-foundation-6': {
-      css: true,
-      js: true
-    }
   });
 
   // Use `app.import` to add additional libraries to the generated
