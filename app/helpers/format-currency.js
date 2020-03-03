bae92768fb40b7437f7d0ce324ab940a139d0f5e
Bruno Roberto Gonçalves <goncalvesb@gmail.com>
2020-03-03 15:36:20 -0300

    fix currency formatter with tests

--- app/helpers/format-currency.js
+++ app/helpers/format-currency.js
@@ -1,18 +1,15 @@
 import { helper } from '@ember/component/helper';
 
-export function formatCurrency(value, namedArgs) {
-  if (isNaN(value)) {
+export default helper(function formatCurrency([value = 0], { sign = '$'} /*, hash*/) {
+  if (!value || isNaN(value)) {
     value = 0;
   }
 
-  let sign = namedArgs.sign === undefined ? '$' : namedArgs.sign;
   let remainder = parseInt((value * 100) % 100, 10);
   let intPart = parseInt(value, 10);
 
   if (remainder.toString().length === 1) {
-    remainder = '0' + remainder;
+    remainder = "0" + remainder;
   }
   return `${sign} ${intPart}.${remainder}`;
-}
-
-export default helper(formatCurrency);
+});
