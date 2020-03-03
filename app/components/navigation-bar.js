72fca2fdf403286e8bd781266a5c1642376ed3aa
Bruno Roberto Gonçalves <goncalvesb@gmail.com>
2020-03-03 16:11:47 -0300

    Convert NavigationBar component to simple template

--- app/components/navigation-bar.js
+++
@@ -1,7 +0,0 @@
-import classic from 'ember-classic-decorator';
-import { tagName } from '@ember-decorators/component';
-import Component from '@ember/component';
-
-@classic
-@tagName('nav')
-export default class NavigationBar extends Component {}
