8507df486c68bc67e63611000679196c0120a524
Bruno Roberto Gonçalves <goncalvesb@gmail.com>
2020-03-18 00:36:15 -0300

    Treat errors in product's page interactions

--- app/controllers/products.js
+++ app/controllers/products.js
@@ -1,9 +1,28 @@
 import Controller from "@ember/controller";
 import { action } from "@ember/object";
+import { tracked } from "@glimmer/tracking";
+import { inject as service } from "@ember/service";
 
 export default class ProductsController extends Controller {
+  @service messaging;
+  @tracked collapsed = true;
+
+  @action
+  toggle() {
+    this.collapsed = !this.collapsed;
+  }
+
+  @action
+  openEditor() {
+    this.collapsed = false;
+  }
+
   @action
   remove(record) {
-    return record.destroyRecord();
+    let messaging = this.messaging;
+
+    return record.destroyRecord().catch(function(reason) {
+        messaging.addError(reason);
+      });
   }
 }
