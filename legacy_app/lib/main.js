bc3a911351ec75bd9a0b0730769aa651fc4018d6
Bruno Roberto Gonçalves <goncalvesb@gmail.com>
2020-03-27 09:22:55 -0300

    All code already converted

--- legacy_app/lib/main.js
+++
@@ -1,6 +0,0 @@
-require('ejr/core');
-require('ejr/templates');
-require('ejr/store');
-require('ejr/router');
-require('ejr/controllers');
-require('ejr/views');
--- legacy_java/main/java/ember/sample/manager/CustomerManager.java
+++
@@ -1,7 +0,0 @@
-package ember.sample.manager;
-
-import ember.sample.entity.Customer;
-
-public class CustomerManager extends Manager<Customer, Long> {
-    
-}
